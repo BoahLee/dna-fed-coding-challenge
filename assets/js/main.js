a4910b5d64ea06fbec2db7df814a9fafafc40c12
Boah Lee <boahlee@hotmail.com>
2020-10-12 13:24:53 +1100

    complete the challenge

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('🙂');
+$(document).ready(function () {
+  $(".slider-container").not(".slick-initialized").slick({
+    infinite: true,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    fadeSpeed: 1000,
+    nextArrow: '<i class="fas fa-chevron-right slick-next"></i>',
+    prevArrow: '<i class="fas fa-chevron-left slick-prev"></i>',
+  });
+});
